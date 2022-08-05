74e8c82b1748c1479bc9dca31beef79a4f457178
Dominic LoBue <dominic.lobue@gmail.com>
2022-08-05 14:38:32 -0700

    Ensure pylsp and bashls are always installed

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -211,3 +211,14 @@ local yamlcompanion = require("yaml-companion").setup({
   -- },
 })
 lspconfig.yamlls.setup(yamlcompanion)
+
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches.
+-- Add your language server below:
+local servers = { 'bashls', 'pylsp' }
+
+-- Call setup
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {}
+end
