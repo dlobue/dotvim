26db67d5b01f78cbe373967045e9e51a5f54c0cc
Dominic LoBue <dominic.lobue@gmail.com>
2022-08-24 13:23:17 -0700

    init updates
    - fix formatting on neogit
    - get conditional loading on firenvim working
    - update doom-one config for its refactor

--- init.lua
+++ init.lua
@@ -74,11 +74,12 @@ require('packer').startup(function(use, use_rocks)
     end
   }
 
-  use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim',
+  use {
+    'TimUntersberger/neogit',
+    requires = 'nvim-lua/plenary.nvim',
     config = function()
-      require('neogit').setup {
-      }
-    end
+      require('neogit').setup {}
+    end,
   }
 
   -- git labels
@@ -171,7 +172,13 @@ require('packer').startup(function(use, use_rocks)
   -- Post-install/update hook with call of vimscript function with argument
   use {
     'glacambre/firenvim',
-    -- cond = vim.g.started_by_firenvim,
+    opt = true,
+    cond = function ()
+      return vim.g.started_by_firenvim == true
+    end,
+    config = function()
+      vim.opt.guifont = 'Fira Code:h9'
+    end,
     setup = function()
       vim.g.firenvim_config = {
         localSettings = {
@@ -185,35 +192,71 @@ require('packer').startup(function(use, use_rocks)
   }
   use {
     'NTBBloodbath/doom-one.nvim',
-    config = function()
-      require('doom-one').setup({
-        cursor_coloring = true,
-        terminal_colors = true,
-        italic_comments = true,
-        enable_treesitter = true,
-        transparent_background = false,
-        pumblend = {
-          enable = true,
-          transparency_amount = 20,
-        },
-        plugins_integrations = {
-          neorg = true,
-          barbar = true,
-          bufferline = false,
-          gitgutter = false,
-          gitsigns = true,
-          telescope = true,
-          neogit = true,
-          nvim_tree = true,
-          dashboard = true,
-          startify = true,
-          whichkey = true,
-          indent_blankline = true,
-          vim_illuminate = true,
-          lspsaga = false,
-        },
-      })
+    setup = function()
+        -- Add color to cursor
+        vim.g.doom_one_cursor_coloring = true
+        -- Set :terminal colors
+        vim.g.doom_one_terminal_colors = true
+        -- Enable italic comments
+        vim.g.doom_one_italic_comments = true
+        -- Enable TS support
+        vim.g.doom_one_enable_treesitter = true
+        -- Color whole diagnostic text or only underline
+        vim.g.doom_one_diagnostics_text_color = true
+        -- Enable transparent background
+        vim.g.doom_one_transparent_background = false
+
+        -- Pumblend transparency
+        vim.g.doom_one_pumblend_enable = true
+        vim.g.doom_one_pumblend_transparency = 20
+
+        -- Plugins integration
+        vim.g.doom_one_plugin_neorg = false
+        vim.g.doom_one_plugin_barbar = false
+        vim.g.doom_one_plugin_telescope = true
+        vim.g.doom_one_plugin_neogit = true
+        vim.g.doom_one_plugin_nvim_tree = true
+        vim.g.doom_one_plugin_dashboard = false
+        vim.g.doom_one_plugin_startify = false
+        vim.g.doom_one_plugin_whichkey = true
+        vim.g.doom_one_plugin_indent_blankline = true
+        vim.g.doom_one_plugin_vim_illuminate = true
+        vim.g.doom_one_plugin_lspsaga = true
     end,
+    config = function()
+        vim.cmd("colorscheme doom-one")
+    end,
+
+
+    -- config = function()
+    --   require('doom-one').setup({
+    --     cursor_coloring = true,
+    --     terminal_colors = true,
+    --     italic_comments = true,
+    --     enable_treesitter = true,
+    --     transparent_background = false,
+    --     pumblend = {
+    --       enable = true,
+    --       transparency_amount = 20,
+    --     },
+    --     plugins_integrations = {
+    --       neorg = true,
+    --       barbar = true,
+    --       bufferline = false,
+    --       gitgutter = false,
+    --       gitsigns = true,
+    --       telescope = true,
+    --       neogit = true,
+    --       nvim_tree = true,
+    --       dashboard = true,
+    --       startify = true,
+    --       whichkey = true,
+    --       indent_blankline = true,
+    --       vim_illuminate = true,
+    --       lspsaga = false,
+    --     },
+    --   })
+    -- end,
   }
 
 
